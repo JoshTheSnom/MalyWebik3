22c6ebe9fe1d2bbf5e686194d410f5054e4ff35b
Kreggy <filip.makrlik@gmail.com>
2021-01-10 21:45:00 +0100

    fix again

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,6 @@ import { UserCreateComponent } from './user-create/user-create.component';
 const routes: Routes = [
   {path: 'users', component: UsersComponent},
   {path: 'user/:id', component: UserDetailComponent},
-  {path: 'user', component: UserDetailComponent},
   {path: 'create', component: UserCreateComponent}
 ];
 
--- src/app/excerpt.pipe.ts
+++ src/app/excerpt.pipe.ts
@@ -5,8 +5,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ExcerptPipe implements PipeTransform {
 
-  transform(value: string): string {
-    if(value.length > 8 ) return value.substring(0, 8) + "..."
+  transform(value: string, delka: number, end: string): string {
+    if(value.length > delka ) return value.substring(0, delka) + end;
     else return value;
   }
 
